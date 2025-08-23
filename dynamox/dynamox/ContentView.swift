b55fcae1f1b9fc8b9a6d67e032df077c5347e18c
Sergio <jarasergio@outlook.com>
2025-08-23 17:48:30 -0300

    initial commit

---
+++ dynamox/dynamox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dynamox
+//
+//  Created by sergio jara on 23/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dynamox/dynamox/dynamoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamoxApp.swift
+//  dynamox
+//
+//  Created by sergio jara on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct dynamoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
